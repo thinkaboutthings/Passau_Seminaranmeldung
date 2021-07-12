f58c7f26686b7bc33ee6f0e418cee7192c5e3e27
thinkaboutthings <philipp.radloff@uni-passau.de>
2021-07-12 09:57:01 +0200

    Testdatensatz: Wahrscheinlichkeit für Staatsexamen und Lehramt ist jetzt adjustierbar

--- Seminarplatzverteilung_Experimentierwiese.R
+++ Seminarplatzverteilung_Experimentierwiese.R
@@ -13,14 +13,18 @@ fachsemester <- seq(1,n,by=1)
 data <- data.frame(matrikelnummer,erstw,zweitw,drittw,studiengang,fachsemester)
 rm(drittw,erstw,fachsemester,matrikelnummer,studiengang,zweitw)
 
+#hilfsvektor für staatsexamen und studiengang
+hilf_studiengang <-c(0,0,0,0,0,0,0,0,0,0,0,1,1,1)
+hilf_staatsexamen <-c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1)
+
 #Füllen mit Zufallswerten
 for (i in 1:n){
   data$matrikelnummer[i]<- 41372+i
   data$erstw[i] <- floor(runif(1,1,11))
   data$zweitw[i] <- floor(runif(1,1,11))
   data$drittw[i] <- floor(runif(1,1,11))
-  data$studiengang[i] <- floor(runif(1,0,2))
-  data$staatsexamen[i] <- floor(runif(1,0,2))
+  data$studiengang[i] <- sample(x=hilf_studiengang,size=1,replace=TRUE)
+  data$staatsexamen[i] <- sample(x=hilf_staatsexamen,size=1,replace=TRUE)
   data$fachsemester[i] <- floor(runif(1,0,7))
 }
 rm(i)
