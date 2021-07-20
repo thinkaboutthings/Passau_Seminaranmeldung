a918108d47ae8482cd9e4164537997a311cc36e6
Fink <fink30@ads.uni-passau.de>
2021-07-20 22:46:14 +0200

    Verteilung formuliert; Es fehlt: Übertragen in ursprünglichen Dataframe, Formulierung als String, Übetragung für alle Studierende

--- Seminarplatzverteilung_Experimentierwiese.R
+++ Seminarplatzverteilung_Experimentierwiese.R
@@ -44,7 +44,81 @@ for (i in 1:seminaranzahl){
   assign(paste("Seminar",i, sep = "_"),raw)
 }
 rm(i,names,raw)
+
 #################Zuteilung der Master-Studierenden#####################################################
+
+#####Nur Masterstudierende#####
+data_master <- filter(data, studiengang==1)
+
+##weniger oder mehr als 30 pro Seminar? bei Erstwunsch##
+apply(data_master,MARGIN=2,table)
+
+###mit if noch als String formulieren?###
+
+##wenn mehr als 30##
+Master_Seminar1 <- subset(data_master, erstw==1)
+Seminar_1 <- sample (Master_Seminar1, size=30)
+
+Master_Seminar2 <- subset(data_master, erstw==2)
+Seminar_2 <- sample (Master_Seminar2, size=30)
+
+Master_Seminar3 <- subset(data_master, erstw==3)
+Seminar_3 <- sample (Master_Seminar3, size=30)
+
+Master_Seminar4 <- subset(data_master, erstw==4)
+Seminar_4 <- sample (Master_Seminar4, size=30)
+
+Master_Seminar5 <- subset(data_master, erstw==5)
+Seminar_5 <- sample (Master_Seminar5, size=30)
+
+Master_Seminar6 <- subset(data_master, erstw==6)
+Seminar_6 <- sample (Master_Seminar6, size=30)
+
+Master_Seminar7 <- subset(data_master, erstw==7)
+Seminar_7 <- sample (Master_Seminar7, size=30)
+
+Master_Seminar8 <- subset(data_master, erstw==8)
+Seminar_8 <- sample (Master_Seminar8, size=30)
+
+Master_Seminar9 <- subset(data_master, erstw==9)
+Seminar_9 <- sample (Master_Seminar9, size=30)
+
+Master_Seminar10 <- subset(data_master, erstw==10)
+Seminar_10 <- sample (Master_Seminar10, size=30)
+
+##wenn weniger als 30##
+
+Seminar_1 <- subset(data_master, data_master$erstw==1)
+Seminar_2 <- subset(data_master, data_master$erstw==2)
+Seminar_3 <- subset(data_master, data_master$erstw==3)
+Seminar_4 <- subset(data_master, data_master$erstw==4)
+Seminar_5 <- subset(data_master, data_master$erstw==5)
+Seminar_6 <- subset(data_master, data_master$erstw==6)
+Seminar_7 <- subset(data_master, data_master$erstw==7)
+Seminar_8 <- subset(data_master, data_master$erstw==8)
+Seminar_9 <- subset(data_master, data_master$erstw==9)
+Seminar_10 <- subset(data_master, data_master$erstw==10)
+
+##In Originaltabelle vermerken##
+
+
+
+#####Nur Staatsexamen nächstes Semester#####
+
+data_staatsexamen <- filter(data, data$staatsexamen==1)
+
+
+#####Sortieren nach Semesteranzahl#####
+
+#hier noch filtern? mit data_semester#
+
+arrange (data_semester, -data$fachsemester)
+
+
+
+
+
+
 
 data_wm_1 <-filter(data, data$studiengang==1)
 
