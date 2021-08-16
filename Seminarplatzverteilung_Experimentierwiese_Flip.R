e79dcebf416d8b1721b564a92cac89409e8f014d
thinkaboutthings <86460603+thinkaboutthings@users.noreply.github.com>
2021-08-16 23:16:46 +0200

    Add files via upload

--- Seminarplatzverteilung_Experimentierwiese_Flip.R
+++ Seminarplatzverteilung_Experimentierwiese_Flip.R
@@ -32,16 +32,16 @@ for (i in 1:n){
   data$staatsexamen[i] <- sample(x=hilf_staatsexamen,size=1,replace=TRUE)
   data$fachsemester[i] <- floor(runif(1,0,7))
 }
-rm(i,hilf_staatsexamen,hilf_studiengang)
+rm(i,hilf_staatsexamen,hilf_studiengang,n)
 
 
 ############################################Funktionen#####################################
 
-Zuteilung <- function(Dataframe,gruppe, x) {
+Zuteilung_1 <- function(Dataframe,gruppe, x) {
   helper_all<- Dataframe
   helper_group <- subset(helper_all,sort==gruppe)
   
- #erstwunsch 
+  #erstwunsch 
   helper_1 <- subset(helper_group, Platz_erhalten==0)
   helper_1 <- subset(helper_1, erstw==x)
   
@@ -53,10 +53,17 @@ Zuteilung <- function(Dataframe,gruppe, x) {
   
   
   helper_all <- join_all(list(helper_1, helper_all) , by = 'matrikelnummer', type = 'full')
-  helper_2 <- join_all(list(helper_1, helper_group) , by = 'matrikelnummer', type = 'full')
+
+  return (helper_all)
+  
+}
+
+Zuteilung_2 <- function(Dataframe,gruppe, x) {
+  helper_all<- Dataframe
+  helper_group <- subset(helper_all,sort==gruppe)
   
   #zweitwunsch
-  helper_1 <- subset(helper_2, Platz_erhalten==0)
+  helper_1 <- subset(helper_group, Platz_erhalten==0)
   helper_1 <- subset(helper_1, zweitw==x)
   
   if ((length(helper_1$matrikelnummer))>(30-(sum(helper_all$Platz_erhalten==x)))) {
@@ -66,10 +73,17 @@ Zuteilung <- function(Dataframe,gruppe, x) {
   } 
   
   helper_all <- join_all(list(helper_1, helper_all) , by = 'matrikelnummer', type = 'full')
-    helper_2 <- join_all(list(helper_1, helper_2) , by = 'matrikelnummer', type = 'full')
+
+    return (helper_all)
+  
+}
+
+Zuteilung_3 <- function(Dataframe,gruppe, x) {
+  helper_all<- Dataframe
+  helper_group <- subset(helper_all,sort==gruppe)
   
   #drittwunsch
-  helper_1 <- subset(helper_2, Platz_erhalten==0)
+  helper_1 <- subset(helper_group, Platz_erhalten==0)
   helper_1 <- subset(helper_1, drittw==x)
   
   if ((length(helper_1$matrikelnummer))>(30-(sum(helper_all$Platz_erhalten==x)))) {
@@ -104,12 +118,30 @@ rm(helper_1,helper_2)
 #Master
 
 for (i in 1:10){
-  data <-  Zuteilung(data,"m",i)
+  data <-  Zuteilung_1(data,"m",i)
+}
+rm(i)
+for (i in 1:10){
+  data <-  Zuteilung_2(data,"m",i)
+}
+rm(i)
+for (i in 1:10){
+  data <-  Zuteilung_3(data,"m",i)
 }
 rm(i)
 #Staatsexamen
 for (i in 1:10){
-  data <-  Zuteilung(data,"s",i)
+  data <-  Zuteilung_1(data,"s",i)
+}
+rm(i)
+
+for (i in 1:10){
+  data <-  Zuteilung_2(data,"s",i)
+}
+rm(i)
+
+for (i in 1:10){
+  data <-  Zuteilung_3(data,"s",i)
 }
 rm(i)
 
@@ -117,9 +149,20 @@ rm(i)
 #nach Fachsemester
 for (k in 20:1){
   for (i in 1:10){
-    data <-  Zuteilung(data,k,i)
+    data <-  Zuteilung_1(data,k,i)
   }
 }
+for (k in 20:1){
+  for (i in 1:10){
+    data <-  Zuteilung_2(data,k,i)
+  }
+}
+for (k in 20:1){
+  for (i in 1:10){
+    data <-  Zuteilung_3(data,k,i)
+  }
+}
+rm(i,k)
 
 #####################################Zusammenfassung######################################
 Seminar<-c(0:10)
